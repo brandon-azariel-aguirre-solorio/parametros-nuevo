8ef287b2d750ea965f9da3880f41b5524034aab3
Saulo <sau890p@gmail.com>
2019-11-07 11:22:41 -0600

    cambios

--- parametro/parametro/parametro.cpp
+++ parametro/parametro/parametro.cpp
@@ -1,19 +1,26 @@
 #include <iostream>
 #include <string>
 using namespace std;
-
+//PARAMETROS POR REFERENCIAS 
+//*sicnifica que es un puntero
+void intercambio(int*, int*);
 int main()
 {
-
-
-
-
-
-
-
-
-
-
+	int a, b;
+	cout << "introduce el primer valor: \n";
+	cin >> a;
+	cout << "introduce el segundo valor: \n";
+	cin >> b;
+	cout << endl;
+	cout << "Valor a vale: " << a << " Valor de b: " << b << endl;
+	intercambio(&a, &b);
+	cout << endl << "Despues del intercambio: " << endl << endl;
+	cout << "Valor a vale: " << a << " Valor de b: " << b << endl;
+	system("pause");
+	return 0;
+}
+void intercambio()
+{
+	
 
 }
-
